0b7930f3c54f6bd162745f66be089455f48a00e9
weerayut <aae606@gmail.com>
2020-05-26 11:51:38 +0700

    prasri

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,41 @@ import { LIFFErrorObject } from 'liff-type';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  os: string;
+  language: String;
+  version: String;
+  isInClient: boolean;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.main()    
   }
 
+  main() {
+    liff.init({ liffId: '1654187718-l0z2DKvr'})
+    .then( () => {
+      if ( !liff.isLoggedIn ) {
+        // liff.login()
+
+      } else {
+        // liff.login()
+      }
+
+
+    })
+    .catch( (err: LIFFErrorObject) => console.error(err.message))    
+  }
+  logIn() {
+    liff.login()
+  }
+  logOut() {
+    liff.logout()
+    console.log('logout');
+    
+  }
+
+  
 }
